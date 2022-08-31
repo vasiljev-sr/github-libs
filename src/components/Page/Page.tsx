c68b42cfc6a8cde48809d1f806510ea44a40a9dd
Ravil <ravil.vasilev@yandex.ru>
2022-08-31 16:14:15 +0300

    Добавил слайдел

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -2,12 +2,24 @@ import React, { FC } from 'react';
 import classNames from 'classnames';
 import './Page.css';
 import { ShortInfo } from '@components/ShortInfo/ShortInfo';
+import { Slider } from '@components/Slider/Slider';
 
 export const Page: FC = () => {
   return (
     <main className="page">
       <h1 className={classNames('title', 'page__title')}>Топ популярных Javascript репозиториев</h1>
-      <ShortInfo title={'ada'} description={'asda dddddddddddddd '} stars={180000} forks={12000} />
+      <section className="page__slider">
+        <Slider>
+          <ShortInfo
+            title={'React'}
+            description={'A declarative, efficient, and flexible JavaScript library for building user interfaces.'}
+            stars={180000}
+            forks={12000}
+          />
+          <ShortInfo title={'ada'} description={'asda dddddddddddddd '} stars={180000} forks={12000} />
+          <ShortInfo title={'ada'} description={'asda dddddddddddddd '} stars={180000} forks={12000} />
+        </Slider>
+      </section>
     </main>
   );
 };
